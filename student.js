5ce4fd136c3fc02ac74cc5ab7d235d953c5214b6
Shashwat Anand <shashwatanand1801@gmail.com>
2022-04-24 13:31:47 +0530

    Pushed student html and js

---
+++ student.js
@@ -0,0 +1,76 @@
+// require(['require', 'mysql'], function (require) {
+//     var namedModule = require('mysql');
+// });
+var temp=require('mysql')
+    // var stdNo = 0;
+    // var tbody = document.getElementById('tbody1');
+  
+  
+    // function AddItemsToTable(fname, lname, mob, vehNo){
+    //   let trow = document.createElement("tr");
+    //   let td1 = document.createElement('td');
+    //   let td2 = document.createElement('td');
+    //   let td3 = document.createElement('td');
+    //   let td4 = document.createElement('td');
+    //   let td5 = document.createElement('td');
+  
+  
+  
+    //   td1.innerHTML = ++stdNo;
+    //   td2.innerHTML = fname;
+    //   td3.innerHTML = lname; 
+    //   td4.innerHTML = mob;
+    //   td5.innerHTML = vehNo;
+      
+  
+    //   trow.appendChild(td1);
+    //   trow.appendChild(td2);
+    //   trow.appendChild(td3);
+    //   trow.appendChild(td4);
+    //   trow.appendChild(td5);
+  
+    //   // console.log(trow);
+  
+    //   tbody.appendChild(trow);
+  
+    // }
+  
+  
+    // const conn = mysql.createConnection({
+    //       host: 'localhost',
+    //       user: 'root',
+    //       password: 'mysql',
+    //       database: 'project_management'
+    //   })
+  
+  
+    //   conn.connect()
+  
+      
+    // function AddAllItemsToTable(student){
+    //   stdNo = 0;
+    //   tbody.innerHTML="";
+    //   student.forEach(element => {
+    //     AddItemsToTable(element.sid, element.courses, element.fname, element.lname);
+    //   })
+    // }
+  
+    //   conn.query('select * from student', (err, rows) => {
+    //       if (err) {
+    //           throw err
+    //       }
+  
+    //       console.log('The student table:', rows)
+  
+    //       setTimeout(() =>{
+    //       rows.forEach(row => {
+    //         AddAllItemsToTable(row)
+    //       });
+    //     }, 20000)
+    //   })
+  
+  
+    //   conn.end()
+
+
+ 
