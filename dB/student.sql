3b6737dd2a3c6687b3ae64c4ae93c71b41785b54
hiten123-ui <vidhani.hiten2001@gmail.com>
2022-04-25 23:27:25 +0530

    added queries for project and prof table

--- dB/student.sql
+++ dB/student.sql
@@ -15,3 +15,28 @@ INSERT INTO `project_management`.`student` (`sid`, `courses`, `fname`, `lname`) 
 ALTER TABLE student RENAME COLUMN courses TO branch;
 ALTER TABLE student ADD cgpa FLOAT(4,2);
 ALTER TABLE student ADD pass VARCHAR(30);
+
+--creating student table
+CREATE TABLE `dbms_project`.`project` (
+  `proj_id` VARCHAR(15) NOT NULL,
+  `title` VARCHAR(90) NULL,
+  `type` INT NULL,
+  `description` VARCHAR(150) NULL,
+  `pre_requisite` VARCHAR(40) NULL,
+  PRIMARY KEY (`proj_id`));
+INSERT INTO `dbms_project`.`project` (`proj_id`, `title`, `type`, `description`, `pre_requisite`) VALUES ('1', 'multimodal machine learning', '1', 'apply ML', 'should know c++');
+INSERT INTO `dbms_project`.`project` (`proj_id`, `title`, `type`, `description`, `pre_requisite`) VALUES ('2', 'remote sensing applications', '1', 'apply qgis', 'c++');
+
+ALTER TABLE dbms_project.project  
+MODIFY type varchar(3) ;  
+
+
+CREATE TABLE `dbms_project`.`professor` (
+  `prof_id` VARCHAR(10) NOT NULL,
+  `email` VARCHAR(45) NULL,
+  `f_name` VARCHAR(45) NULL,
+  `l_name` VARCHAR(45) NULL,
+  `password` VARCHAR(33) NULL,
+  PRIMARY KEY (`prof_id`));
+INSERT INTO `dbms_project`.`professor` (`prof_id`, `email`, `f_name`, `l_name`, `password`) VALUES ('1', 'prof@hyderabad.com', 'prof', 'essor', '1234');
+INSERT INTO `dbms_project`.`professor` (`prof_id`, `email`, `f_name`, `l_name`, `password`) VALUES ('2', 'teach@hyderabad.com', 'teach', 'er', '123');
